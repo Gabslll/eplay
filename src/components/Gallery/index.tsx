b85f30fd62f9104f8cdab4a8cc09a82eaaadce4b
Gabriel Ramos <gabsrammus@gmail.com>
2025-07-13 23:26:58 -0300

    Added more description in each product

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,25 @@
+import zelda from '../../assets/images/banner-homem-aranha.png'
+import Section from '../Section'
+
+import { Item, Items } from './styles'
+
+const Gallery = () => (
+    <Section title="Galeria" background="black">
+        <Items>
+          <Item>
+            <img src={zelda} alt="Zelda" />
+          </Item>
+          <Item>
+            <img src={zelda} alt="Zelda" />
+          </Item>
+          <Item>
+            <img src={zelda} alt="Zelda" />
+          </Item>
+          <Item>
+            <img src={zelda} alt="Zelda" />
+          </Item>
+        </Items>
+      </Section>
+)
+
+export default Gallery
