bdf5fe4c1501e803dc9b6036ca668d0896e64fad
Gabriel Ramos <gabsrammus@gmail.com>
2025-07-23 22:51:16 -0300

    Added part of galery images

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,25 +1,81 @@
-import zelda from '../../assets/images/banner-homem-aranha.png'
+import spiderman from '../../assets/images/banner-homem-aranha.png'
+import hogwarts from '../../assets/images/fundo_hogwarts.png'
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+import fechar from '../../assets/images/fechar.png'
 import Section from '../Section'
 
-import { Item, Items } from './styles'
+import { Item, Items, Action, Modal, ModalContent } from './styles'
 
-const Gallery = () => (
-    <Section title="Galeria" background="black">
+type GalleryProps = {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GalleryProps[] = [
+  {
+    type: 'image',
+    url: spiderman
+  },
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/uHGShqcAHlQ?si=kCHiwkqhvqi_S3LB'
+  }
+]
+
+type Props = {
+  defaultCover?: string
+  name?: string
+}
+
+const Gallery = ({ defaultCover, name }: Props) => {
+  const getMediaCover = (item: GalleryProps) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryProps) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  return (
+    <>
+      <Section title="Galeria" background="black">
         <Items>
-          <Item>
-            <img src={zelda} alt="Zelda" />
-          </Item>
-          <Item>
-            <img src={zelda} alt="Zelda" />
-          </Item>
-          <Item>
-            <img src={zelda} alt="Zelda" />
-          </Item>
-          <Item>
-            <img src={zelda} alt="Zelda" />
-          </Item>
+          {mock.map((media, index) => (
+            <Item key={media.url}>
+              <img
+                src={getMediaCover(media)}
+                alt={`Midia ${index + 1} de ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para maximizar a midia"
+                />
+              </Action>
+            </Item>
+          ))}
         </Items>
       </Section>
-)
+
+      <Modal>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img src={fechar} alt="Fechar galeria" />
+          </header>
+          <img src={spiderman} alt="Banner do jogo" />
+        </ModalContent>
+        <div className="overlay"></div>
+      </Modal>
+    </>
+  )
+}
 
 export default Gallery
